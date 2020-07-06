ab3f3f075bc2603a23b3db5fa27c135316de08ae
riyagoel192 <riyagoel192@gmail.com>
2020-07-07 00:24:55 +0530

    files updated

--- register.php
+++ register.php
@@ -2,11 +2,80 @@
 <html>
 <head>
 	<title>Registration Page</title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- main css -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 	<link rel="stylesheet" type="text/css" href="css/registration.css">
+
+	<!-- Bootstrap css -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- JS, Popper.js, and jQuery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </head>
 <body>
-	<div id="main-block">
-		hello
-	</div>
+
+	<div class="upper-menu">
+
+			<img class="logo" src="images/download.png">
+			
+			<div id="navbar">
+				
+				<ul id="nav">
+					<li><a href="#" style="text-decoration: none;">HOME</a></li>
+					<li><a href="#" style="text-decoration: none;">GALLERY</a></li>
+					<li><a href="#" style="text-decoration: none;">ABOUT</a></li>
+					<li><a href="#" style="text-decoration: none;" onmouseover="showmenu()" onmouseout="hidemenu()">EVENTS
+						<div id="dropdown-events">
+							<div class="triangle-up"></div>
+							<ul id="drop">
+								<li>Social Events</li>
+								<li>Event Details</li>
+								<li>Event Matches</li>
+							</ul>
+						</div>
+
+					</a></li>
+					<li><a href="#" style="text-decoration: none;">CONTACT</a></li>
+				</ul>
+
+			</div>
+
+			<button type="button" class="btn btn-danger" onclick="redirect()">Register as Volunteer</button>
+
+		</div>
+
+		<div id="main-block">
+		
+		</div>
+
 </body>
 </html>
+
+<script>
+
+	// function redirect()
+	// {
+	// 	//alert("hello user");
+	// 	window.location.href = "register.php"
+	// }
+
+	function showmenu()
+	{
+		document.getElementById('dropdown-events').style.display="block";
+	}
+
+	function hidemenu()
+	{
+		document.getElementById('dropdown-events').style.display="none";
+	}
+
+</script>
